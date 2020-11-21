009102826a12715cc662bf05a6d8e56178837a4c
ThomasPellegrini <pellegrinithomas1@gmail.com>
2020-11-21 23:46:36 +0100

    Add skill bars

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,13 +1,47 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Content from "../components/Content";
+import SkillBar from 'react-skillbars';
 
 import youtube from "../assets/images/youtube.png"
 
 function AboutPage(props){
 
+    const skills = [
+        {
+            "type": "Python",
+            "level": 80
+
+        },
+        {
+            "type": "C++",
+            "level": 50
+        },
+        {
+            "type": "Docker",
+            "level": 45
+        },
+        {
+            "type": "Java",
+            "level": 40
+        },
+        {
+            "type": "Javascript",
+            "level": 25
+        }
+    ]
+    const colors = {
+        "bar": "#09d6e8",
+        "title": {
+            "hue":  9,
+            "text": "#fff",
+            "background": "#2795e3",
+            "saturation": 20
+        }
+    }
+
     return(
-        <div>
+        <div >
             <Hero title={props.title} />
 
             <Content>
@@ -17,7 +51,9 @@ function AboutPage(props){
                 <p>This is the part where i talk about me blablabla. This is the part where i talk about me blablabla.</p>
                 <p>This is the part where i talk about me blablabla. This is the part where i talk about me blablabla.</p>
                 <p>This is the part where i talk about me blablabla. This is the part where i talk about me blablabla.f</p>
-
+                <div className={'skill'}>
+                    <SkillBar   skills={skills} height={40} colors={colors}/>
+                </div>
             </Content>
 
 {/*
